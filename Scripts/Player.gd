88b00a1c5263cb4912f128d10e3cca1b752e7cf2
Nathan Michel <nathannkombe@gmail.com>
2024-07-10 18:51:20 +0100

    Update Player.gd

--- Scripts/Player.gd
+++ Scripts/Player.gd
@@ -1,30 +1,42 @@
 extends CharacterBody2D
 
-var speed : int = 200 
-@onready var animated_sprite = $AnimatedSprite2D
-var Velocity = Vector2(0,0)
+const SPEED = 300
+const JUMP_VELOCITY = -1200
+var gravity = 2000
+@onready var player_animation = $AnimatedSprite2D
 
-func _player_movement(delta): 
-	
-	if Input.is_action_pressed("ui_left"):
-		Velocity.x -= speed
-		animated_sprite.play("run")
-		animated_sprite.flip_h = true
-	if Input.is_action_pressed("ui_right"):
-		Velocity.x += speed
-		animated_sprite.flip_h = false
-	if Input.is_action_pressed("ui_up"):
-		Velocity.y -= speed
-		animated_sprite.play("jump")
-	elif not is_on_floor():
-		animated_sprite.play("jump")
-	else:
-		Velocity.x = 0 
-		Velocity.y = 0
-		animated_sprite.play("idle")
+func _physics_process(delta): 
+	if Input.is_action_pressed("ui_left") :
+		velocity.x = -SPEED
+		player_animation.play("run")
+		player_animation.flip_h = true
+	elif Input.is_action_pressed("ui_right"): 
+		velocity.x = SPEED
+		player_animation.play("run")
+		player_animation.flip_h = false
+	else :
+		player_animation.play("idle")
+	if not is_on_floor():
+		player_animation.play("jump")
+		velocity.y = velocity.y + gravity * delta
+	if Input.is_action_just_pressed("ui_up") and is_on_floor() : 
+		velocity.y = JUMP_VELOCITY
+		player_animation.play("jump")  
+	if not is_on_floor():
+		player_animation.play("jump")
+		velocity.y = velocity.y + gravity * delta
+		player_animation.play("idle")    
 		
-		move_and_slide()
-		velocity.x = lerp(velocity.x,0,0.1)
+		
+	velocity.x = lerp(velocity.x,0.0,0.1)
+	move_and_slide()  
+		
+	 
+	
+	
+	
+	
+	
 
 	
 	
